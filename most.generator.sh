dfc82a8df667ee51556f7ef08fc2cf00ed8e3868
Lara CODECA <lara.codeca@eurecom.fr>
2019-05-09 21:53:30 +0200

    Configuration files for MoST Scenario v0.6 and 20k population example.

--- most.generator.sh
+++ most.generator.sh
@@ -27,7 +27,7 @@ MOBILITY_TOOLS="$MOBILITY_GENERATOR/MoSTScenario/tools/mobility"
 INPUT="$SCENARIO/in"
 ADD="$INPUT/add"
 
-OUTPUT="out"
+OUTPUT="rou"
 mkdir -p $OUTPUT
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PUBLIC TRANSPORTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
@@ -38,19 +38,24 @@ echo "[$(date)] --> Generate bus trips..."
 python $SUMO_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 900 \
     --random-begin --seed 42 --no-vtypes \
     --ptstops $ADD/most.busstops.add.xml --ptlines $MOBILITY_TOOLS/pt/most.buslines.add.xml \
-    -o $OUTPUT/test.buses.flows.xml
+    -o $OUTPUT/most.example.buses.flows.xml
 
-sed -e s/:0//g -i $OUTPUT/test.buses.flows.xml
+sed -e s/:0//g -i $OUTPUT/most.example.buses.flows.xml
 
 echo "[$(date)] --> Generate train trips..."
 python $SUMO_TOOLS/ptlines2flows.py -n $INPUT/most.net.xml $INTERVAL -p 1200 \
     -d 300 --random-begin --seed 42 --no-vtypes \
     --ptstops $ADD/most.trainstops.add.xml --ptlines $MOBILITY_TOOLS/pt/most.trainlines.add.xml \
-    -o $OUTPUT/test.trains.flows.xml
+    -o $OUTPUT/most.example.trains.flows.xml
 
-sed -e s/:0//g -i $OUTPUT/test.trains.flows.xml
+sed -e s/:0//g -i $OUTPUT/most.example.trains.flows.xml
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TRACI MOBILITY GENERATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 echo "[$(date)] --> Generate mobility..."
 python3 activitygen.py -c most.activitygen.json
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SUMO SIMULATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+echo "[$(date)] --> Running the SUMO simulation..."
+sumo -c most.test.sumocfg
