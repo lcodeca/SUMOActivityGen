073940adc012df5e266f59412c67b1eb0f9dde09
Lara CODECA <lara.codeca@gmail.com>
2021-12-18 16:14:02 +0000

    saga setup.py

---
+++ saga/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="SAGA",
+    version="0.1",
+    description="Activity-based Multi-modal Mobility Scenario Generator for SUMO",
+    url="https://github.com/lcodeca/SUMOActivityGen",
+    author="Lara CODECA",
+    author_email="lara.codeca@gmail.com",
+    license="EPLv2",
+    packages=["src"],
+    zip_safe=False,
+)
